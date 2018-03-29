d385aef35a5081340af57456eb71dcd3e6ea23e0
sparrow <280669537@qq.com>
2018-03-29 14:35:43 +0800

    Read just part of audio signal if cant read over one pass. Specify the range of axis. :pear: sparrowli

--- readWave02.m
+++ readWave02.m
@@ -1,7 +1,11 @@
 fileName='welcome.wav';
-[y, fs, nbits]=wavread(fileName);
+[y, fs, nbits]=wavread(fileName, [5001 6000]);
 
+time = (1:length(y))/fs;
+y = y * (2^nbits/2);
 fprintf('音讯文件"%s"的信息:\n', fileName);
 fprintf('音讯长度 = %g 秒\n', length(y)/fs);
 fprintf('采样频率 = %g Hz\n', fs);
 fprintf('采样精度 = %g 位元/采样点\n', nbits);
+%figure;
+plot(time, y);axis([min(time), max(time), -inf, inf]);
